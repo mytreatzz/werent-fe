2c45fbac457161e7a4d2f5571beeba85ef9ffd0c
enrico-zd <enricozada00@gmail.com>
2025-12-03 14:25:26 +0700

    feat: add CardProduct component

---
+++ src/components/product/CardProduct.tsx
@@ -0,0 +1,135 @@
+import StarRate from "../review/StarRate";
+import { fetchProduct } from "@/service/ProductAPI";
+import Link from "next/link";
+import { fetchReviewSummary } from "@/service/ReviewAPI";
+import { totalDistValue } from "@/utils/totalDistValue";
+
+export default async function ProductDetail() {
+  // const [reviews, setReviews] = useState(initialReviews)
+
+  const dataProduct = await fetchProduct(2);
+  const dataReviewSummary = await fetchReviewSummary(2);
+
+  return (
+    <div className="min-h-screen bg-white">
+      {/* navigation */}
+      <nav></nav>
+      {/* Product Image Section */}
+      <div className="relative bg-gray-100">
+        <div className="h-96 flex items-center justify-center overflow-hidden">
+          <img
+            src={dataProduct.imageUrl}
+            alt={dataProduct.name}
+            className="object-contain"
+          />
+        </div>
+      </div>
+
+      {/* Content Section */}
+      <div className="max-w-4xl mx-auto px-6 py-6 lg:px-0">
+        {/* Product Title & Rating */}
+        <div className="mb-4">
+          <h1 className="text-2xl font-bold">{dataProduct.name}</h1>
+          <p className="text-xl font-semibold text-gray-700">
+            Rp {dataProduct.price?.toLocaleString("id-ID")}
+          </p>
+          <div className="flex items-center gap-1 my-4">
+            <StarRate rating={dataProduct.overallRating} />
+            <span className="text-gray-400 text-[11px]">
+              {dataProduct.totalReviews} REVIEWS
+            </span>
+          </div>
+          <p className="text-gray-500 leading-relaxed max-w-2xl">
+            {dataProduct.description}
+          </p>
+        </div>
+
+        {/* Size */}
+        <div className="flex flex-col">
+          <button className="text-sm justify-start w-fit pb-2">
+            AVAILABLE SIZE
+          </button>
+          <div>
+            {dataProduct.sizes.map((size, i) => (
+              <button
+                key={i}
+                className="text-sm w-10 py-2 mr-2 text-center border hover:outline hover:outline-offset-4"
+              >
+                {size}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        <div className="border-t-2 border-gray-300 my-3" />
+
+        {/* Reviews Section */}
+        <div className="mb-8">
+          <div className="flex items-center justify-between mb-6">
+            <h2 className="text-xl font-bold">
+              REVIEWS ({dataReviewSummary.totalReviews})
+            </h2>
+            <Link
+              href="/reviews"
+              className="text-green-700 font-semibold hover:underline"
+            >
+              View More &gt;
+            </Link>
+          </div>
+
+          {/* Average Rating */}
+          <div className="mb-8">
+            <div className="flex items-center gap-2 mb-4">
+              <StarRate rating={dataReviewSummary.overallRating} />
+            </div>
+          </div>
+
+          {/* Size Distribution */}
+          <div className="space-y-3 mb-8">
+            {Object.entries(dataReviewSummary.fitDistribution).map(
+              ([key, value]) => (
+                <div key={key} className="flex items-center gap-4">
+                  <span className="w-24 text-sm font-medium">{key}</span>
+                  <div className="flex-1 bg-gray-300 rounded-full h-3 overflow-hidden">
+                    <div
+                      className="bg-green-700 h-full"
+                      style={{
+                        width: `${
+                          (value /
+                            totalDistValue(dataReviewSummary.fitDistribution)) *
+                          100
+                        }%`,
+                      }}
+                    />
+                  </div>
+                  <span className="w-12 text-sm text-gray-600 text-right">
+                    {Math.round(
+                      (value /
+                        totalDistValue(dataReviewSummary.fitDistribution)) *
+                        100
+                    )}
+                    %
+                  </span>
+                </div>
+              )
+            )}
+          </div>
+
+          <div className="border-t border-gray-300 my-8" />
+
+          {/* Individual Reviews */}
+          {/* <div className="space-y-8">
+            {reviews.map((review) => (
+              <div key={review.id}>
+
+                <CardReview 
+                    review={review}
+                />
+              </div>
+            ))}
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+}
