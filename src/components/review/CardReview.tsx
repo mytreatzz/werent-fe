91d32e023e8e5d9e013c30bdf239b90e4fec99e1
enrico-zd <enricozada00@gmail.com>
2025-12-03 15:06:02 +0700

    fix: restore bagian kode yang hilang setelah penyelesaian merge conflict

--- src/components/review/CardReview.tsx
+++ src/components/review/CardReview.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { FaThumbsUp } from "react-icons/fa";
 import StarRate from "./StarRate";
@@ -8,8 +8,15 @@ import { Review } from "@/types";
 const CardReview = ({ review }: { review: Review }) => {
   const [dataReview, setDataReview] = useState(review);
   const [likedReviews, setLikedReviews] = useState<Set<number>>(new Set());
-  
+
+  const [reviewFullView, setReviewFullView] = useState(false);
+  const [imageView, setImageView] = useState<string | null>(null);
+
+  const reviewText = review.content || "";
+  const longReviewText = reviewText.length > 150;
+
   const handleLikeClick = (reviewId: number) => {
+
     const isLiked = likedReviews.has(reviewId);
     const newLikedReviews = new Set(likedReviews);
 
@@ -40,11 +47,11 @@ const CardReview = ({ review }: { review: Review }) => {
           <div>
             {/* beri icon 5 bintang */}
             <p>{review.user.name}</p>
-            
 
             {/* data body size contoh: 165cm 65kg 88/78/110 cm */}
             <p className="text-[11px] text-gray-400">
-              {review.userMeasurement.bust} / {review.userMeasurement.waist} / {review.userMeasurement.hips} CM
+              {review.userMeasurement.bust} / {review.userMeasurement.waist} /{" "}
+              {review.userMeasurement.hips} CM
             </p>
           </div>
         </div>
@@ -68,14 +75,46 @@ const CardReview = ({ review }: { review: Review }) => {
       </div>
 
       <div className="flex py-2">
-              <StarRate rating={review.rating}/>
-            </div>
+        <StarRate rating={review.rating} />
+      </div>
 
       {/* text review dari user */}
+      <div>
+        {reviewFullView ? reviewText : reviewText.substring(0, 150)}
+        {!reviewFullView && longReviewText && "..."}
+
+        {longReviewText && (
+          <button onClick={() => setReviewFullView(!reviewFullView)}>
+            {reviewFullView ? "Show Less" : " Read More"}
+          </button>
+        )}
+      </div>
 
       {/* field gambar dari product review */}
+      {review.media && review.media.length > 0 && (
+        <div>
+          {review.media.map((media, index) => (
+            <img
+              key={index}
+              src={media.url}
+              alt={`Review Image ${index}`}
+              onClick={() => setImageView(media.url)}
+            />
+          ))}
+        </div>
+      )}
+      <div>
+        {imageView && (
+          <div onClick={() => setImageView(null)}>
+            <div>
+              <img src={imageView} alt="Full Preview" />
+            </div>
+          </div>
+        )}
+      </div>
 
       {/* bulan, tanggal dan tahun dari review */}
+      <div>{review.createdAt}</div>
     </div>
   );
 };
